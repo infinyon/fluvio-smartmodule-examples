d10961bd4a05372d3ea4b79939a3e3bdc7e03274
Nick Mosher <nicholastmosher@gmail.com>
2021-11-30 15:04:27 -0500

    Initial commit for github connector

---
+++ crates/github-stars-smartmodule/src/lib.rs
@@ -0,0 +1,14 @@
+use fluvio_smartmodule::{smartmodule, Result};
+use fluvio_smartmodule::extract::*;
+use fluvio_model_github::GhRepo;
+use serde_json::{json, Value as JsonValue};
+
+#[smartmodule(map)]
+pub fn map(record: Value<Json<GhRepo>>) -> Result<Value<Json<JsonValue>>> {
+    let repo = record.inner();
+
+    Ok(Value(Json(json!({
+        "stars": repo.stargazers_count,
+        "watchers": repo.watchers_count,
+    }))))
+}
